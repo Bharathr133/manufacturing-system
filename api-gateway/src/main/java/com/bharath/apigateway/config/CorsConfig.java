9304e47ce9819a7464036cf82469469213062866
Bharath <Bharath@STPLAL54.swajyot.local>
2026-06-05 16:31:37 +0530

    Fix CORS for Vercel

--- api-gateway/src/main/java/com/bharath/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/bharath/apigateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        config.setAllowedOrigins(List.of("https://manufacturing-system-six.vercel.app/","http://localhost:5173", "http://127.0.0.1:5173"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setMaxAge(3600L);
